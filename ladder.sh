845097fe0d132434cd9d0814cc1d77e9f86b489a
vinay <vinayvip19@gmail.com>
2022-01-30 17:01:43 +0530

    ladder has finished

---
+++ ladder.sh
@@ -0,0 +1,26 @@
+#ladder if
+if [condition]
+then
+elif [condition]
+then
+elif [condition]
+fi
+
+#If else type
+if [condition]
+then
+fi
+
+if [condition]
+then
+
+else
+
+fi
+
+if [condition]
+then
+
+elif [condition]
+
+fi
